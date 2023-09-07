accf9d1b509849febf4756e6c7fd850de45b0a70
MatCat96 <215802@studenti.unimore.it>
2023-09-07 15:31:23 +0200

    fixed printing position

--- src/performance_eval.cpp
+++ src/performance_eval.cpp
@@ -218,7 +218,7 @@ void Controller::stop()
     if (SAVE_LOGS)
     {
         std::cout << "Final position of the robots: \n";
-        for (int i = 0; i < ROBOT_RANGE; i++)
+        for (int i = 0; i < ROBOTS_NUM; i++)
         {
             std::string text = std::to_string(this->pose_x(i)) + " " + std::to_string(this->pose_y(i)) + " " + std::to_string(this->pose_theta(i)) + "\n";
             write_log_file(text);
